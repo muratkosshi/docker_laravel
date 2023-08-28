0bb6a0170580d0a930420d07d6ee96d7d45654c4
murat.kosshi <murat.kosshi@gmail.com>
2023-08-28 18:40:55 +0500

    add controller post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,11 +20,56 @@ class PostController extends Controller
               'title' => 'title of post from phpstorm',
               'content' =>'some interesting content',
               'image' =>'imageblabla.jpg',
-              'likes' =>20,
+              'likes' =>50,
+              'is_published' => 1,
+
+          ],
+          [
+              'title' => 'title of post from phpstorm',
+              'content' =>'some interesting content',
+              'image' =>'imageblabla.jpg',
+              'likes' =>50,
               'is_published' => 1,
 
           ]
         ];
 
+
+        foreach($postsArr as $item)
+        {
+            Post::create(
+                [
+                    'title' => $item['title'],
+                    'content' =>$item['content'],
+                    'image' =>$item['image'],
+                    'likes' =>$item['likes'],
+                    'is_published' => $item['is_published'],
+                ]
+            );
+        }
+        dd('created');
+
+
+    }
+
+    public function update(){
+        $post = Post::find(6);
+        $post -> update(
+            [
+                'title' =>'updated1231',
+                'content' =>'updated',
+                'image' =>'updated',
+                'likes' =>1000,
+                'is_published' =>1,
+            ]
+        );
+        dd($post->title);
+
+    }
+    public function delete()
+    {
+        $post = Post::find(6);
+        $post -> delete();
+        dd('deleted post');
     }
 }
